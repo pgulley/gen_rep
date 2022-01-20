77fc79a411e3dff80178ff55966930df8c7c4836
paige <nano3.14@gmail.com>
2022-01-19 19:48:54 -0500

    oops

---
+++ upload_frontend/static/groups.js
@@ -0,0 +1,45 @@
+
+Vue.component('task-group', {
+  	props:["group"],
+  	template: `<div class="task-group" v-bind:id=group.id>
+			<h2><a :href=href>{{ group.title }}</a></h2>
+
+			<div class="group-description">
+				{{ group.description}}
+			</div>
+			</div>`,
+	data:function(){
+		return {
+			id:this.group.id,
+			href: get_url(`/group/${this.group.id}`)
+		}
+	}
+
+})
+
+
+function get_url(target){
+	stage = $("body").attr("stage")
+	if(stage == "None"){
+		return target
+	}else{
+		return "/"+stage+target
+	}
+}
+
+get_groups = function(){
+	
+	$.ajax({
+		url:get_url("/all_groups")
+	}).done(function(resp){
+		console.log(resp)
+		var app1 = new Vue({
+			el:"#task-groups",
+			data:{
+			grouplist:resp.data
+			}
+		})
+		vueapp = app1
+	})
+}
+get_groups()
